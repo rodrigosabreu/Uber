befd74a91e56f115a9fe0e9e42ed70790f28b4fe
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-12-13 15:59:48 -0200

    Autenticacao do usuario no Firebase

--- Uber/ViewController/EntrarViewController.swift
+++ Uber/ViewController/EntrarViewController.swift
@@ -7,9 +7,72 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EntrarViewController: UIViewController {
 
+    @IBOutlet var email: UITextField!
+    @IBOutlet var senha: UITextField!
+    
+    
+    @IBAction func entrar(_ sender: Any) {
+        
+        let retorno = self.validarCampos()
+        
+        if retorno == ""{
+            
+            if let emailR = self.email.text{
+                if let senhaR = self.senha.text{
+                    
+                    //Faz autenticacao do usuario (login)
+                    let autenticacao = Auth.auth()
+                    autenticacao.signIn(withEmail: emailR, password: senhaR, completion: { (usuario, erro) in
+                        
+                        if erro == nil{
+                            
+                            //verificar se o usuario esta logado
+                            if usuario != nil{
+                                
+                                print("Usuário foi autenticado com sucesso")
+                                
+                            }
+                            
+                            
+                            
+                        }else{
+                            print("Erro ao autenticar o usuario, tente novamente.")
+                        }
+                        
+                        
+                    })
+                    
+                }
+            }
+            
+        }else{
+            print("O campo \(retorno) não foi preenchido!")
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    func validarCampos() -> String {
+        
+        if (self.email.text?.isEmpty)!{
+            return "E-mail"
+        }else if (self.senha.text?.isEmpty)!{
+            return "Senha"
+        }
+        
+        return ""
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
