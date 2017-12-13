710b6434e9113d53a40c92ae0a298f06e7a372de
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-12-13 09:40:35 -0200

    Initial Commit

---
+++ Uber/ViewController/EntrarViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EntrarViewController.swift
+//  Uber
+//
+//  Created by Rodrigo Abreu on 13/12/2017.
+//  Copyright © 2017 Rodrigo Abreu. All rights reserved.
+//
+
+import UIKit
+
+class EntrarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
